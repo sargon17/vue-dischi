0f74edd0828adac4b1fdc0c7e25b7b5f2c8bd6f3
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-04-08 16:26:44 +0200

    Working on header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from "./components/HeaderComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderComponent,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/sass/styles.scss";
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="primary" class="header">
+      <b-navbar-brand>
+        <img
+          class="logo"
+          src="http://1000logos.net/wp-content/uploads/2017/08/Spotify-Logo.png"
+          alt="Spotyfy logo"
+        />
+      </b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import { BNavbar, BNavbarBrand } from "bootstrap-vue";
+
+export default {
+  name: "HeaderComponent",
+  components: {
+    BNavbar,
+    BNavbarBrand,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/sass/styles.scss";
+
+.header {
+  background-color: $gray-800 !important;
+
+  .logo {
+    width: 100px;
+  }
+}
+</style>
